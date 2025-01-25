374937e55bb0926aca2cd3f152f15c52b44ffcf7
TenTakano <takano20771@gmail.com>
2025-01-26 01:00:40 +0900

    Install Nostrum

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule DiscordBot.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:nostrum, "~> 0.10"}
     ]
   end
 
