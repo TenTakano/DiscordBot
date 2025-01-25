ef072f53d357e88f297683c8f8f08017061ac6e0
TenTakano <takano20771@gmail.com>
2025-01-25 23:32:45 +0900

    Create Phoenix Project

---
+++ lib/discord_bot_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule DiscordBotWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use DiscordBotWeb, :controller` and
+  `use DiscordBotWeb, :live_view`.
+  """
+  use DiscordBotWeb, :html
+
+  embed_templates "layouts/*"
+end
