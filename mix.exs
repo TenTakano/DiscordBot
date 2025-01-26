78527b3f444902e306e1fa2fcab6cffaa7c40e93
TenTakano <takano20771@gmail.com>
2025-01-27 02:36:30 +0900

    [chore] Update HeroIcons

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule DiscordBot.MixProject do
       {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
       {:heroicons,
        github: "tailwindlabs/heroicons",
-       tag: "v2.1.1",
+       tag: "v2.1.5",
        sparse: "optimized",
        app: false,
        compile: false,
