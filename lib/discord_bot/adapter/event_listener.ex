dd892ffe40ce6d8131725847ef53c04c4df16ddb
TenTakano <takano20771@gmail.com>
2025-01-28 20:59:07 +0900

    Implement Get Global Ip Function Call Logic

--- lib/discord_bot/adapter/event_listener.ex
+++ lib/discord_bot/adapter/event_listener.ex
@@ -2,8 +2,21 @@ defmodule DiscordBot.Adapter.EventListener do
   use Nostrum.Consumer
 
   alias Nostrum.Api
+  alias DiscordBot.Adapter.Ip
 
   def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
+    case msg.content do
+      "!ip" ->
+        Api.create_message(msg.channel_id, Ip.get_global_ip())
+
+      _ ->
+        handle_with_regular_expression(msg)
+    end
+  end
+
+  def handle_event(_event), do: :ignore
+
+  def handle_with_regular_expression(msg) do
     case DiscordBot.Adapter.Dice.filter_and_roll_dice(msg.content) do
       result when is_binary(result) ->
         Api.create_message(msg.channel_id, "#{msg.author.username}: #{result}")
@@ -12,6 +25,4 @@ defmodule DiscordBot.Adapter.EventListener do
         :ignore
     end
   end
-
-  def handle_event(_event), do: :ignore
 end
---
+++ lib/discord_bot/adapter/ip.ex
@@ -0,0 +1,5 @@
+defmodule DiscordBot.Adapter.Ip do
+  def get_global_ip() do
+    "123.456.789.123"
+  end
+end
