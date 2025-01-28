ab7967369df06832e4f01ab5e99db67331b80f48
TenTakano <takano20771@gmail.com>
2025-01-29 01:41:44 +0900

    Remove Unused Function and Implement Test for Dice Module

--- lib/discord_bot/adapter/dice.ex
+++ lib/discord_bot/adapter/dice.ex
@@ -1,16 +1,6 @@
 defmodule DiscordBot.Adapter.Dice do
   alias DiscordBot.RandomUtil
 
-  def filter_and_roll_dice(content) do
-    normalized_downcase = String.normalize(content, :nfkc) |> String.downcase()
-
-    with %{"count" => count, "sides" => sides} <-
-           Regex.named_captures(~r/^(?<count>\d+)d(?<sides>\d+)$/, normalized_downcase) do
-      total = roll_dice(String.to_integer(count), String.to_integer(sides))
-      "(#{count}d#{sides}) -> #{total}"
-    end
-  end
-
   def capture_dice_roll(message) do
     normalized_downcase = String.normalize(message, :nfkc) |> String.downcase()
     Regex.named_captures(~r/^(?<count>\d+)d(?<sides>\d+)$/, normalized_downcase)
