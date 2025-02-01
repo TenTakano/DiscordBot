55231316d4e002bfbadc251eeb01ca27abbd6f34
TenTakano <takano20771@gmail.com>
2025-02-01 21:36:24 +0900

    Append Reset Parameter

--- lib/discord_bot_web/controllers/llm_controller.ex
+++ lib/discord_bot_web/controllers/llm_controller.ex
@@ -5,12 +5,23 @@ defmodule DiscordBotWeb.LlmController do
 
   @total_cost_per_million_tokens 10
 
-  def report_monthly_cost(conn, _params) do
-    tokens = Llm.get_total_usage()
-    usd_cost = Float.ceil(tokens / 1_000_000) * @total_cost_per_million_tokens
+  def report_monthly_cost(conn, params) do
+    with {:ok, %{reset: reset}} <- validate_params(params) do
+      tokens = Llm.get_total_usage()
+      usd_cost = Float.ceil(tokens / 1_000_000) * @total_cost_per_million_tokens
 
-    :ok = Llm.reset_total_usage()
+      if reset do
+        :ok = Llm.reset_total_usage()
+      end
 
-    json(conn, %{tokens: tokens, usd_cost: usd_cost})
+      json(conn, %{tokens: tokens, usd_cost: usd_cost})
+    else
+      {:error, :bad_request} ->
+        put_status(conn, :bad_request) |> json(%{error: "Invalid parameters"})
+    end
   end
+
+  defp validate_params(%{"reset" => reset}) when is_boolean(reset), do: {:ok, %{reset: reset}}
+  defp validate_params(%{"reset" => _reset}), do: {:error, :bad_request}
+  defp validate_params(_params), do: {:ok, %{reset: false}}
 end
--- test/discord_bot_web/controllers/llm_controller_test.exs
+++ test/discord_bot_web/controllers/llm_controller_test.exs
@@ -2,9 +2,11 @@ defmodule DiscordBotWeb.LlmControllerTest do
   use DiscordBotWeb.ConnCase
 
   describe "POST /api/v1/llm/report-monthly-cost" do
-    test "returns the total tokens and the USD cost", %{conn: conn} do
-      path = "/api/v1/llm/report-monthly-cost"
+    setup %{conn: conn} do
+      [conn: conn, path: "/api/v1/llm/report-monthly-cost"]
+    end
 
+    test "returns the total tokens and the USD cost", %{conn: conn, path: path} do
       conn = post(conn, path)
       assert json_response(conn, 200) == %{"tokens" => 0, "usd_cost" => 0.0}
 
@@ -12,6 +14,19 @@ defmodule DiscordBotWeb.LlmControllerTest do
 
       conn = post(conn, path)
       assert json_response(conn, 200) == %{"tokens" => 10, "usd_cost" => 10.0}
+    end
+
+    test "resets the total tokens if the reset parameter is true", %{conn: conn, path: path} do
+      DiscordBot.Llm.upsert_usage(10)
+      conn = post(conn, path, %{reset: true})
+
+      assert json_response(conn, 200) == %{"tokens" => 10, "usd_cost" => 10.0}
+      assert DiscordBot.Llm.get_total_usage() == 0
+    end
+
+    test "returns an error if the reset parameter is not a boolean", %{conn: conn, path: path} do
+      conn = post(conn, path, %{reset: "true"})
+      assert json_response(conn, 400) == %{"error" => "Invalid parameters"}
     end
   end
 end
