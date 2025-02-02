1bcd1444db926a686af069cdee042eeeb7914e0d
TenTakano <takano20771@gmail.com>
2025-02-03 03:22:56 +0900

    Refactor chat_with_model/1

--- lib/discord_bot/llm.ex
+++ lib/discord_bot/llm.ex
@@ -8,28 +8,32 @@ defmodule DiscordBot.Llm do
     history = History.generate_initial_history(message)
     tools = get_tool_functions() |> Enum.map(& &1.definition)
 
-    case OpenAIClient.ask_model(history, tools: tools) do
-      {:stop, message, usage} ->
-        upsert_usage(usage["total_tokens"])
+    chat_with_model_repeatedly(history, tools)
+  end
+
+  defp chat_with_model_repeatedly(history, tools) do
+    {reason, message, usage} = OpenAIClient.ask_model(history, tools: tools)
+    upsert_usage(usage["total_tokens"])
+
+    case {reason, message} do
+      {:stop, message} ->
         %{content: message["content"], total_tokens: usage["total_tokens"]}
 
-      {:tool_calls, message, usage} ->
-        upsert_usage(usage["total_tokens"])
-
+      {:tool_calls, message} ->
         history =
           Enum.reduce(
             message["tool_calls"],
             History.append_tool_call(history, message),
-            fn tool_call, acc ->
-              result = "134.7.25.83"
-              History.append_tool_result(acc, tool_call["id"], result)
-            end
+            &execute_tool_function/2
           )
 
-        {:stop, message, usage} = OpenAIClient.ask_model(history, tools: tools)
-        upsert_usage(usage["total_tokens"])
-        %{content: message["content"], total_tokens: usage["total_tokens"]}
+        chat_with_model_repeatedly(history, tools)
     end
+  end
+
+  defp execute_tool_function(tool_call, history) do
+    tool_result = "134.7.25.83"
+    History.append_tool_result(history, tool_call["id"], tool_result)
   end
 
   def get_total_usage() do
