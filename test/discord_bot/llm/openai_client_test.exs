3db5540c39d3689958f8f6757e57b8cd6d2db5b2
TenTakano <takano20771@gmail.com>
2025-02-03 02:52:32 +0900

    Add Test

--- test/discord_bot/llm/openai_client_test.exs
+++ test/discord_bot/llm/openai_client_test.exs
@@ -22,5 +22,23 @@ defmodule DiscordBot.Llm.OpenAiClientTest do
 
       assert OpenAIClient.ask_model([], []) == {:stop, "response", %{"total_tokens" => 10}}
     end
+
+    test "returns response from OpenAI API with tools" do
+      expect(Mock, :post!, 1, fn _endpoint, body ->
+        assert {:ok, json} = Keyword.fetch(body, :json)
+        assert json["tools"] == ["tool"]
+
+        %{
+          status: 200,
+          body: %{
+            "choices" => [%{"finish_reason" => "stop", "message" => "response"}],
+            "usage" => %{"total_tokens" => 10}
+          }
+        }
+      end)
+
+      assert OpenAIClient.ask_model([], tools: ["tool"]) ==
+               {:stop, "response", %{"total_tokens" => 10}}
+    end
   end
 end
