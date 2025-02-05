fdfffc7b045f2db93054e54b5e526d3b67849b91
TenTakano <takano20771@gmail.com>
2025-02-06 01:48:28 +0900

    Delete Unused Code

--- lib/discord_bot_web/controllers/auth_controller.ex
+++ lib/discord_bot_web/controllers/auth_controller.ex
@@ -2,13 +2,6 @@ defmodule DiscordBotWeb.AuthController do
   use DiscordBotWeb, :controller
   plug Ueberauth
 
-  alias Ueberauth.Strategy.Helpers
-
-  def request(conn, _params) do
-    IO.inspect(Helpers.request_url(conn))
-    redirect(conn, to: Helpers.request_url(conn))
-  end
-
   def callback(%{assigns: %{ueberauth_failure: %Ueberauth.Failure{}}} = conn, _params) do
     conn
     |> put_flash(:error, "Failed to authenticate")
