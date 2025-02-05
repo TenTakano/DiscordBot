86c0101a2925180108de2b9d49c31db7e914259e
TenTakano <takano20771@gmail.com>
2025-02-06 02:52:31 +0900

    Implement Auth Callback Logic

--- lib/discord_bot_web/controllers/auth_controller.ex
+++ lib/discord_bot_web/controllers/auth_controller.ex
@@ -2,19 +2,41 @@ defmodule DiscordBotWeb.AuthController do
   use DiscordBotWeb, :controller
   plug Ueberauth
 
+  alias DiscordBot.Accounts
+
+  def callback(%{assigns: %{ueberauth_auth: %Ueberauth.Auth{} = auth}} = conn, _params) do
+    auth
+    |> generate_account_params()
+    |> Accounts.find_or_create_account()
+    |> case do
+      {:ok, account} ->
+        conn
+        |> put_session(:account_id, account.id)
+        |> put_flash(:info, "Successfully authenticated")
+        |> redirect(to: "/")
+
+      {:error, _changeset} ->
+        conn
+        |> put_flash(:error, "Failed to authenticate")
+        |> redirect(to: "/")
+    end
+  end
+
   def callback(%{assigns: %{ueberauth_failure: %Ueberauth.Failure{}}} = conn, _params) do
     conn
     |> put_flash(:error, "Failed to authenticate")
     |> redirect(to: "/")
   end
 
-  def callback(%{assigns: %{ueberauth_auth: _auth}} = conn, _params) do
-    # TODO: Implement this
-    user = %{id: "12345"}
-
-    conn
-    |> put_session(:user_id, user.id)
-    |> put_flash(:info, "Successfully authenticated")
-    |> redirect(to: "/")
+  defp generate_account_params(auth) do
+    %{
+      uid: auth.uid,
+      provider: Atom.to_string(auth.provider),
+      name: auth.info.nickname,
+      avatar: auth.info.image,
+      token: auth.credentials.token,
+      refresh_token: auth.credentials.refresh_token,
+      expires_at: DateTime.from_unix!(auth.credentials.expires_at)
+    }
   end
 end
